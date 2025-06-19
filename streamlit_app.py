a2e6f7236a634cb1f96a0e1b921b33cd1e007268
shakiravier <adindapermta@gmail.com>
2025-06-19 21:46:35 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
-st.title("My website")
+st.title("My website.")
 st.write(
-    "Terimakasih sudah mampir bos")
-st.header("hai shalova")
+    "Terimakasih sudah mampir bos.")
+st.header("hai shalova🤓")
 st.image("IMG-20250619-WA0100.jpg")
